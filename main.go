fec79257fbc5df10220b03eefa57b6d7464fb97f
Randall Sturgis <randall@sturgis.io>
2017-02-13 22:28:35 -0800

    flush out structs

--- main.go
+++ main.go
@@ -3,40 +3,110 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"os"	
+	"os"
+)
+
+const HELP_TEXT = "\n" +
+"game is a tool for managing goals.\n" +
+"\n" +
+"commands:\n" +
+"\tadd\tadd a reward or challenge\n" +
+"\tremove\tremove a reward or challenge\n" +
+"\tlist\tlist rewards, challenges, or ledger entries\n" +
+"\tedit\tedit a reward, challenge, or ledger entry\n" +
+"\tq\tquit"
+
+type transactionType int
+
+const (
+	Credit transactionType = iota
+	Debit
+)
+
+type timescale int
+
+const (
+	Day timescale = iota
+	Week
+	Month
+	Year
 )
 
 type Transaction struct {
-	property int
+	entryType transactionType
+	itemType string
+	amount int
+	points int
+	description string
 }
 
 type Reward struct {
-	property int
+	weight float64
+	unit string
+	amount int
+	timescale timescale
 }
 
 type Challenge struct {
-	property int
+	weight float64
+	unit string
+	amount int
+	timescale timescale
 }
 
 type User struct {
 	username string
 	ledger []Transaction
 	balance int
-	rewards map[string]Reward
-	challenge map[string]Challenge
 }
 
 type Game struct {
 	user User
+	rewards map[string]Reward
+	challenge map[string]Challenge
 }
 
 func (g Game) Init(u string) {
 	fmt.Println("initializing game...")
 	g.user.username = u
+	g.user.balance = 1000000
 }
 
-func (g Game) Command(c string) {
-	fmt.Println("you typed: ", c)
+func (g Game) Command(c string, s *bufio.Scanner) {
+	switch c {
+	case "add":
+		g.Add(s)
+	//case "remove":
+	//	g.Remove(s)
+	//case "list":
+	//	g.List(s)
+	//case "edit":
+	//	g.Edit(s)
+	case "help":
+		g.Help()
+	default:
+		fmt.Println("command not recognized...")
+		g.Help()
+	}
+}
+
+func (g Game) Add(s *bufio.Scanner) {
+	fmt.Println("add a reward or challenge?")
+	s.Scan()
+	input := s.Text()
+	switch input {
+	case "reward":
+		fmt.Println("you chose to add a reward")
+	case "challenge":
+		fmt.Println("you chose to add a challenge")
+	default:
+		fmt.Println("type \"reward\" or \"challenge\".")
+		g.Add(s)
+	}
+}
+
+func (g Game) Help() {
+	fmt.Println(HELP_TEXT)
 }
 
 func main() {
@@ -51,7 +121,7 @@ func main() {
 		if input == "q" {
 			break
 		}
-		g.Command(input)
+		g.Command(input, s)
 		fmt.Print("command: ")
 	}
 }
